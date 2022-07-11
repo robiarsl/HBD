ea0de6264490d71c9afc6504938d51ea00b36e09
robiarsl <robiarsala102@gmail.com>
2022-07-11 23:07:11 +0700

    musik

--- effect.js
+++ effect.js
@@ -6,7 +6,7 @@ $("document").ready(function () {
   var vw;
   $(window).resize(function () {
     vw = $(window).width() / 2;
-    $("#b1,#b2,#b3,#b4,#b5,#b6,#b7,#b8,#b9").stop();
+    $("#b1,#b2,#b3,#b4,#b5,#b6,#b7,#b8").stop();
     $("#b11").animate({ top: 240, left: vw - 350 }, 500);
     $("#b22").animate({ top: 240, left: vw - 250 }, 500);
     $("#b33").animate({ top: 240, left: vw - 150 }, 500);
